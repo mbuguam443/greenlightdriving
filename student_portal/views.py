97db15e94d742a166dc2467a7ad983448da8b0e1
Mathew Mbugua <mbuguam443@gmail.com>
2026-08-08 17:00:46 +0300

    fix: import messages in post method

--- student_portal/views.py
+++ student_portal/views.py
@@ -74,6 +74,7 @@ class PortalLessonsView(StudentRequiredMixin, View):
     def get(self, request):
         from students.models import Student
         from lessons.models import PracticalLesson, TheoryLesson, LessonItem
+        from django.contrib import messages
         try:
             student = Student.objects.get(user=request.user)
         except Student.DoesNotExist:
