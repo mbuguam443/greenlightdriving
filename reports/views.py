5b1f0851bf8d54e820f3811783ec5a4dfc920de2
Mathew Mbugua <mbuguam443@gmail.com>
2026-08-10 08:56:29 +0300

    fix: missing redirect import in reports views

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.views import View
